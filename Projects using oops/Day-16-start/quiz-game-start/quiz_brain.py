e3d0b9b9c05c35919c3f7ce8ef497acc67044bf3
Anurag <aroraanurag647@gmail.com>
2024-11-11 18:51:16 +0530

    day 18

--- Projects using oops/Day-16-start/quiz-game-start/quiz_brain.py
+++ Projects using oops/Day-16-start/quiz-game-start/quiz_brain.py
@@ -2,6 +2,7 @@ class QuizBrain:
     def __init__(self,question_list):
         self.ques_num=0
         self.ques_list=question_list
+        self.score=0
     def still_has_questions(self):
         return self.ques_num < len(self.ques_list)
            
@@ -9,8 +10,17 @@ class QuizBrain:
     def next_question(self):
         current_question=self.ques_list[self.ques_num]
         self.ques_num+=1
-        input(f"{self.ques_num}: {current_question.text} (True/False): ")
-   
+        user_answer=input(f"{self.ques_num}: {self.current_question.text} (True/False): ")
+        self.check_answer(user_answer,current_question.answer)
+
+    def check_answer(self,user_answer,correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("you got it right")
+        else:
+            print('Thats Wrong')
+        print(f"The correct answer was: {correct_answer}  ")        
+             
+        
         
             
            
